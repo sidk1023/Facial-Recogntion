176cc04b8cc6e04a4d2c36c692bcc674d6f5e26c
sidk1023 <skolipara5@gmail.com>
2023-01-27 12:51:20 +0530

    Full pipeline with txt output

--- Code/pipeline.py
+++ Code/pipeline.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+import time
 from frameDiff import FrameDiff
 from faceDetect import FaceDetect
 from faceRecog import FaceRecog
@@ -20,8 +21,17 @@ prev_frame = cv2.resize(curr_frame,(600, 400))
 print("read first frame")
 ret, curr_frame = cap.read()
 curr_frame = cv2.resize(curr_frame,(600, 400))
+frameCount = 2
+frameSkip = 3
+noFaceCount = 0
+multipleFaceCount = 0
+unverifiedFaceCount = 0
+outputStr = "{}, {}, {}\n"
+file = open("pipeline_output.txt","w")
+start_time =time.time()
 while True:
     try:
+        frame_input_time = time.time()
         diff_val, diff_score = diff.ssim(prev_frame, curr_frame)
         if diff_val:
             cv2.putText(curr_frame, 'Frame Change', (0,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
@@ -34,9 +44,6 @@ while True:
                 x, y, w, h = int(x), int(y), int(w), int(h)
                 text = f"{conf[0]*100:.2f}%"
                 face_frame = cv2.resize(curr_frame[y-50:h+50,x-50:w+50],(1000,1000))
-                
-                # cv2.imshow("frame2",face_frame)
-                # cv2.imshow("frame3",user)
                 cv2.putText(curr_frame,text,(x, y - 20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0, 255, 0),1)
                 cv2.rectangle(curr_frame, (x, y), (w, h), (0, 255, 0), 1)
                 isSame, distance = rec.verify(curr_frame,user)
@@ -46,16 +53,26 @@ while True:
                 else:
                     cv2.putText(curr_frame, 'Face Not Verified', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
                     print("face is not verified", distance)
-
+                    file.write(outputStr.format("Face Not Verified",frameCount,time.time()-start_time))
+                    unverifiedFaceCount+=1
 
                 
             elif numFaces==0:
                 print("0 face detected")
                 cv2.putText(curr_frame, 'No Faces', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                file.write(outputStr.format("No Faces Detected",frameCount,time.time()-start_time))
+                noFaceCount+=1
             else:
                 print("multiple faces detected")
                 cv2.putText(curr_frame, 'Multiple Faces', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                file.write(outputStr.format("Multiple Faces Detected",frameCount,time.time()-start_time))
+                multipleFaceCount+=1
         cv2.imshow('frame',curr_frame)
+        processing_time = time.time()
+        time_diff = processing_time - frame_input_time
+        time_total = processing_time -start_time
+        frameCount+=frameSkip
+        cap.set(cv2.CAP_PROP_POS_FRAMES,frameCount)
         prev_frame = curr_frame.copy()
         ret, curr_frame = cap.read()
         curr_frame = cv2.resize(curr_frame,(600,400))
@@ -63,7 +80,11 @@ while True:
         break
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
-
+file.write("Summary\n")
+file.write("1. Missing Face: "+str(noFaceCount)+"\n")
+file.write("2. Multiple Faces in Frame: "+str(multipleFaceCount)+"\n")
+file.write("3. Unverified User: "+ str(unverifiedFaceCount)+"\n")
+file.close()
 cap.release()
 cv2.destroyAllWindows()
     
